b74c86c9dfcbb6d9df995fad29eaa81d4955dabb
Radim Štěpaník <radim.stepanik@gmail.com>
2021-02-09 10:40:03 +0100

    🐛 GF-4181 - ab tests should not be tracked on server

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
-const main = async () => {
-    const a = {
-        b: {
-            c: {
-                test: 'test',
-            },
-        },
-    };
-    console.log(a?.b);
+import bodyParser from 'body-parser';
+import express from 'express';
+import mustacheExpress from 'mustache-express';
 
-    return;
-};
+const app = express();
 
-main();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.engine('mustache', mustacheExpress());
+
+app.set('view engine', 'mustache');
+app.set('views', `${__dirname}/views`);
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/', async (req, res) => {
+    res.render('index');
+
+});
+
+app.listen(8000);
+
+// const sleep =
